e19808788165027caa0aa674fbf0eb4a8b386d40
Martin <mespinal@techassist.mx>
2020-09-29 18:20:37 -0500

    set dinamyc piece to borad

--- Diagramador_ajedrez/src/vista/interfaz.py
+++ Diagramador_ajedrez/src/vista/interfaz.py
@@ -78,66 +78,69 @@ button.pack()
 button.place(x=400, y=60)
 
 def setPiece(pice):
+    print("--- " + pice)
     tablero.setPiece(pice)
+
+
 
 # Definición de las piezas
 alfil_blanco = ImageTk.PhotoImage(Image.open(path + "Abb.jpg"))
-alfil_b = tk.Button(ventana, image = alfil_blanco, command = setPiece("Abb.jpg"))
+alfil_b = tk.Button(ventana, image = alfil_blanco, command = lambda: setPiece("Abb.jpg"))
 alfil_b.pack()
 alfil_b.place(x = 200, y = 500)
 
 alfil_negro = ImageTk.PhotoImage(Image.open(path + "Anb.jpg"))
-alfil_n = tk.Button(ventana, image = alfil_negro, command = setPiece("Anb.jpg"))
+alfil_n = tk.Button(ventana, image = alfil_negro, command = lambda: setPiece("Anb.jpg"))
 alfil_n.pack()
 alfil_n.place(x = 200, y = 100)
 
 caballo_blanco = ImageTk.PhotoImage(Image.open(path + "Cbb.jpg"))
-caballo_b = tk.Button(ventana, image = caballo_blanco, command = setPiece("Cbb.jpg"))
+caballo_b = tk.Button(ventana, image = caballo_blanco, command = lambda: setPiece("Cbb.jpg"))
 caballo_b.pack()
 caballo_b.place(x = 250, y = 500)
 
 caballo_negro = ImageTk.PhotoImage(Image.open(path + "Cnb.jpg"))
-caballo_n = tk.Button(ventana, image = caballo_negro, command = setPiece("Cnb.jpg"))
+caballo_n = tk.Button(ventana, image = caballo_negro, command = lambda: setPiece("Cnb.jpg"))
 caballo_n.pack()
 caballo_n.place(x = 250, y = 100)
 
 torre_blanco = ImageTk.PhotoImage(Image.open(path + "Tbb.jpg"))
-torre_b = tk.Button(ventana, image = torre_blanco, command = setPiece("Tbb.jpg"))
+torre_b = tk.Button(ventana, image = torre_blanco, command = lambda: setPiece("Tbb.jpg"))
 torre_b.pack()
 torre_b.place(x = 300, y = 500)
 
 torre_negro = ImageTk.PhotoImage(Image.open(path + "Tnb.jpg"))
-torre_n = tk.Button(ventana, image = torre_negro, command = tablero.setPiece("Tnb.jpg"))
+torre_n = tk.Button(ventana, image = torre_negro, command = lambda: setPiece("Tnb.jpg"))
 torre_n.pack()
 torre_n.place(x = 300, y = 100)
 
 peon_blanco = ImageTk.PhotoImage(Image.open(path + "Pbb.jpg"))
-peon_b = tk.Button(ventana, image = peon_blanco, command = tablero.setPiece("Pbb.jpg"))
+peon_b = tk.Button(ventana, image = peon_blanco, command = lambda: setPiece("Pbb.jpg"))
 peon_b.pack()
 peon_b.place(x = 350, y = 500)
 
 peon_negro = ImageTk.PhotoImage(Image.open(path + "Pnb.jpg"))
-peon_n = tk.Button(ventana, image = peon_negro, command = tablero.setPiece("Pnb.jpg"))
+peon_n = tk.Button(ventana, image = peon_negro, command = lambda: setPiece("Pnb.jpg"))
 peon_n.pack()
 peon_n.place(x = 350, y = 100)
 
 dama_blanco = ImageTk.PhotoImage(Image.open(path + "Dbb.jpg"))
-dama_b = tk.Button(ventana, image = dama_blanco, command = tablero.setPiece("Dbb.jpg"))
+dama_b = tk.Button(ventana, image = dama_blanco, command = lambda: setPiece("Dbb.jpg"))
 dama_b.pack()
 dama_b.place(x = 400, y = 500)
 
 dama_negro = ImageTk.PhotoImage(Image.open(path + "Dnb.jpg"))
-dama_n = tk.Button(ventana, image = dama_negro, command = tablero.setPiece("Dnb.jpg"))
+dama_n = tk.Button(ventana, image = dama_negro, command = lambda: setPiece("Dnb.jpg"))
 dama_n.pack()
 dama_n.place(x = 400, y = 100)
 
 rey_blanco = ImageTk.PhotoImage(Image.open(path + "Rbb.jpg"))
-rey_b = tk.Button(ventana, image = rey_blanco, command = tablero.setPiece("Rbb.jpg"))
+rey_b = tk.Button(ventana, image = rey_blanco, command = lambda: setPiece("Rbb.jpg"))
 rey_b.pack()
 rey_b.place(x = 450, y = 500)
 
 rey_negro = ImageTk.PhotoImage(Image.open(path + "Rnb.jpg"))
-rey_n = tk.Button(ventana, image = rey_negro, command = tablero.setPiece("Rnb.jpg"))
+rey_n = tk.Button(ventana, image = rey_negro, command = lambda: setPiece("Rnb.jpg"))
 rey_n.pack()
 rey_n.place(x = 450, y = 100)
 

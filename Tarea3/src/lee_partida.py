4ab4fc694b68cef50ebc62a85915eba1a7fb77a8
Martin <mespinal@techassist.mx>
2020-10-15 20:22:18 -0500

    parser complete

--- Tarea3/src/lee_partida.py
+++ Tarea3/src/lee_partida.py
@@ -3,13 +3,12 @@ import parser as pgn
 def lee():
     archivo = open('nuevo-london.pgn','r', encoding='utf-8', errors='ignore')
     lista = archivo.readlines()
-    numlin=0
 
     for linea in lista:
         # incrementa en 1 el contador  
-        numlin += 1
         # muestra contador y elemento (línea)
-        print(numlin, linea)
+        print(linea)
+        print(type(linea))
     
     archivo.close()  # cierra archivo
 lee()
